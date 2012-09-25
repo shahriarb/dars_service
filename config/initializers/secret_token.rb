0195ee9f22085730a8e902d81acf5029004fafde
shahriarb <shahriar.b@gmail.com>
2012-09-25 13:24:59 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DarsService::Application.config.secret_token = '0ea9b01970e8f3f6486a30fe311edf0223e65b09a0f3bc03d79832793a6b1b6fc6fe593f913f4b4020b38be7f763a9387da2db6b07598c873428c6b83f2e816f'
