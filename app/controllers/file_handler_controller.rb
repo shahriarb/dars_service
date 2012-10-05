1597dd0c7d3522a2ae06f18a88e271b4e0fc82c4
shahriarb <shahriar.b@gmail.com>
2012-10-05 14:18:20 +0200

    read binary file

--- app/controllers/file_handler_controller.rb
+++ app/controllers/file_handler_controller.rb
@@ -6,11 +6,12 @@ class FileHandlerController < ApplicationController
   GetMyFileResult = Struct.new(:resultcode,:filecontent)
   def getmyfile
   	
-  	s = Base64.encode64(File.read(File.join(Rails.root,"app/assets/testfile.txt"))) 
-    
+  	#s = Base64.encode64(File.read(File.join(Rails.root,"app/assets/testfile.txt"))) 
+    contents = open(File.join(Rails.root,"app/assets/testfile.txt"), "rb") {|io| io.read }
 
+    contents = Base64.encode64(contents)
     respond_to do |format|
-        format.json { render json: GetMyFileResult.new(0,s) }
+        format.json { render json: GetMyFileResult.new(0,contents) }
     end
   end
 end
